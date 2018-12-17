afa7cee837e0e97b8195c0149e53153a0dd36041
Halley Rebecca Young <halleyy@chestnut-login.internal>
2018-12-17 15:28:01 -0500

    added files

---
+++ gencycle4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --get-user-env
+#SBATCH --partition=g_deviet
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20000M 
+
+srun python train.py --dataroot ./datasets/facades_third --name facades_third_pix2pix --model pix2pix --display_id 0 --batch_size 2 --gpu_ids 0 --input_nc 3 --output_nc 3 --direction AtoB
